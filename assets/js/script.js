f1c3cc50b899a77e19e003a7e52535cfd0320aa3
Chris_Gruen <113554860+ChristyGruen@users.noreply.github.com>
2023-07-15 12:48:47 -0500

    homework mvp complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,15 +28,15 @@ var localTimeHour = dayjs().hour();
 //need to add dayjs advancedFormat and use Do to get suffixes
 //https://day.js.org/docs/en/plugin/advanced-format
 var localDate = dayjs().format('dddd, MMMM D');
-var savedText9 = localStorage.getItem('txt9');
-var savedText10 = localStorage.getItem('txt10');
-var savedText11 = localStorage.getItem('txt11');
-var savedText12 = localStorage.getItem('txt12');
-var savedText13 = localStorage.getItem('txt13');
-var savedText14 = localStorage.getItem('txt14');
-var savedText15 = localStorage.getItem('txt15');
-var savedText16 = localStorage.getItem('txt16');
-var savedText17 = localStorage.getItem('txt17');
+// var savedText9 = localStorage.getItem('txt9');
+// var savedText10 = localStorage.getItem('txt10');
+// var savedText11 = localStorage.getItem('txt11');
+// var savedText12 = localStorage.getItem('txt12');
+// var savedText13 = localStorage.getItem('txt13');
+// var savedText14 = localStorage.getItem('txt14');
+// var savedText15 = localStorage.getItem('txt15');
+// var savedText16 = localStorage.getItem('txt16');
+// var savedText17 = localStorage.getItem('txt17');
 
 console.log(localDate)
 console.log(localTimeHour)
@@ -44,7 +44,7 @@ console.log(localTimeHour)
 $('#currentDay').text(localDate)
 
  
-$('#txt9').text(savedText9)
+// $('#txt9').text(savedText9)
 
 
 // $('#9').css( "border", "3px solid red" );
@@ -57,15 +57,20 @@ $('#txt9').text(savedText9)
 //if calendar > localTimeHour, addClass('future');
 for (i = 9;i<18;i++){
   if (localTimeHour > $(`#hour-${i}`).attr('value')){
-    $(`#hour-${i}`).addClass('past'),
-    savedText[i] = localStorage.getItem(`txt${i}`);
-    $(`#txt${i}`).text(savedText[i])
+    $(`#hour-${i}`).addClass('past')
+    $(`#hour-${i}.children[1]`).addClass('gerbil')
+    let toothpaste = localStorage.getItem(`txt${i}`);
+    $(`#txt${i}`).text(toothpaste)
   }
   else if(localTimeHour == $(`#hour-${i}`).attr('value')){
     $(`#hour-${i}`).addClass('present')
+    let hairspray = localStorage.getItem(`txt${i}`);
+    $(`#txt${i}`).text(hairspray)
   }
   else if(localTimeHour < $(`#hour-${i}`).attr('value')){
     $(`#hour-${i}`).addClass('future')
+    let shampoo = localStorage.getItem(`txt${i}`);
+    $(`#txt${i}`).text(shampoo)
   }
   else {
     console.log('not during work')
@@ -110,4 +115,80 @@ button9.on('click', function () {
 
   console.log($('#btn9').attr('id')); 
 
+});
+
+
+//try on hour-10
+var button10 = $('button#btn10');
+
+//create function what to do when button is clicked  on = eventListener
+button10.on('click', function () {
+  console.log( $(this)); 
+  var hour10Text = $("textarea#txt10").val()
+  console.log(hour10Text)
+  localStorage.setItem('txt10',hour10Text);
+  console.log($('#btn10').attr('id')); 
+});
+
+var button11 = $('button#btn11');
+button11.on('click', function () {
+  console.log( $(this)); 
+  var hour11Text = $("textarea#txt11").val()
+  console.log(hour11Text)
+  localStorage.setItem('txt11',hour11Text);
+  console.log($('#btn11').attr('id')); 
+});
+
+var button12= $('button#btn12');
+button12.on('click', function () {
+  console.log( $(this)); 
+  var hour12Text = $("textarea#txt12").val()
+  console.log(hour12Text)
+  localStorage.setItem('txt12',hour12Text);
+  console.log($('#btn12').attr('id')); 
+});
+
+var button13 = $('button#btn13');
+button13.on('click', function () {
+  console.log( $(this)); 
+  var hour13Text = $("textarea#txt13").val()
+  console.log(hour13Text)
+  localStorage.setItem('txt13',hour13Text);
+  console.log($('#btn13').attr('id')); 
+});
+
+var button14 = $('button#btn14');
+button14.on('click', function () {
+  console.log( $(this)); 
+  var hour14Text = $("textarea#txt14").val()
+  console.log(hour14Text)
+  localStorage.setItem('txt14',hour14Text);
+  console.log($('#btn14').attr('id')); 
+});
+
+var button15 = $('button#btn15');
+button15.on('click', function () {
+  console.log( $(this)); 
+  var hour15Text = $("textarea#txt15").val()
+  console.log(hour15Text)
+  localStorage.setItem('txt15',hour15Text);
+  console.log($('#btn15').attr('id')); 
+});
+
+var button16 = $('button#btn16');
+button16.on('click', function () {
+  console.log( $(this)); 
+  var hour16Text = $("textarea#txt16").val()
+  console.log(hour16Text)
+  localStorage.setItem('txt16',hour16Text);
+  console.log($('#btn16').attr('id')); 
+});
+
+var button17 = $('button#btn17');
+button17.on('click', function () {
+  console.log( $(this)); 
+  var hour17Text = $("textarea#txt17").val()
+  console.log(hour17Text)
+  localStorage.setItem('txt17',hour17Text);
+  console.log($('#btn17').attr('id')); 
 });
